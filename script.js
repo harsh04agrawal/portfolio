563a1bf73f05de327037ab75c7952aa4beb97c0a
Harsh Agrawal <harshagrawal845@gmail.com>
2024-08-24 11:35:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Toggle mobile menu
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('nav a');
+
+navToggle.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.querySelector('nav').classList.remove('nav-open');
+  });
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Script to dynamically update the Last Updated date
+document.addEventListener("DOMContentLoaded", function () {
+    const updateDateElement = document.getElementById('update-date');
+    const lastUpdatedDate = new Date(2024, 7, 23); // Month is 0-indexed (August is 7)
+    const formattedDate = lastUpdatedDate.toLocaleDateString('en-GB');
+    updateDateElement.textContent = formattedDate;
+  });
+  
